de46113ff83851dbed13749a390a9970672b041b
Thainara066 <127572393+Thainara066@users.noreply.github.com>
2023-10-06 10:04:25 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,42 +1,46 @@
 <?php
 
-class calculadora
+class Calculadora
 {
 	public function somar(float|int $valor1, float|int...$valores):float|int
-{
-	foreach ($valores as $valor){
-		$valor1 += $valor;
-	}
-	return $valor1;
-	}
-	public function subtrair(float|int $num1,float|int...$valores):float|int
 	{
-	foreach ($valores as $valor){
-		$num1 -= $valor;
+		foreach ($valores as $valor) {
+			$valor1 += $valor;
+		}
+		return $valor1;
 	}
-	return $num1;
+	public function subtrair(float|int $num1, float|int...$valores):float|int
+	{
+		foreach ($valores as $valor) {
+			$num1 -= $valor;
+		}
+
+		return $num1;
+
 	}
-	
+
 	public function dividir (float|int $v1, float|int... $valores):float|int
 	{
-	foreach ($valores as $valor){
-		$v1 /= $valor;
+		foreach ($valores as $valor) {
+			$v1 /= $valor;
+		}
+		return $v1;
 	}
-	return $v1;
-	}
-	
+
 	public function multiplicar (float|int $n1, float|int... $valores):float|int
 	{
-foreach ($valores as $valor){
-		$n1 *= $valor;
+		foreach ($valores as $valor) {
+			$n1 *= $valor;
+		}
+		return $n1;
 	}
-	return $n1;	}
 }
-$calculadora = new calculadora ;
-$soma=$calculadora->somar(5,2,-10);
-$subtracao=$calculadora->subtrair(-90,30,-50);
-$divisao=$calculadora->dividir(100,5,5);
-$multiplicacao=$calculadora ->multiplicar(8,2,-2);
+
+$calculadora = new calculadora;
+$soma = $calculadora->somar(5, 2, -10);
+$subtracao = $calculadora->subtrair(90, 30, -50);
+$divisao = $calculadora->dividir(100, 5, 5);
+$multiplicacao = $calculadora ->multiplicar(8, 2, -2);
 
 echo ("O resultado da soma é = $soma"). PHP_EOL;
 echo (" O resultado da subtração é = $subtracao") . PHP_EOL;
